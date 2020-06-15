884181bd03b57da3f9aecf16cc8ab2f9be191e03
Max Lamb <maxlambcodes@gmail.com>
2020-06-15 16:44:07 -0700

    bid creation tested and working

---
+++ __tests__/bid.test.js
@@ -0,0 +1,79 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
+const Bid = require('../lib/models/Bid');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('bid routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let user;
+  let bid;
+  let auction;
+
+  beforeEach(async() => {
+    user = await User.create({
+      email: 'steve@castiel.com',
+      password: 'salmondean'
+    });
+    
+    auction = await Auction.create({
+      user: user.id,
+      title: 'Cheap Sneakers',
+      description: 'Crappy Shoes',
+      quantity: 2,
+      endDate: Date.now()
+    });
+    bid = await Bid.create({
+      user: user.id,
+      auction: auction.id,
+      price: 205,
+      quantity: 1,
+      accepted: true
+    });
+
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('creates a new bid', async() => {
+    
+    return request(app)
+      .post('/api/v1/bid')
+      .send({
+        auction: auction._id,
+        user: user._id,
+        price: 5,
+        quantity: 1,
+        accepted: true
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          id: expect.anything(),
+          user: user.id,
+          auction: auction.id,
+          quantity: 1,
+          accepted: true,
+          price: 5,
+          __v: 0
+        });
+      });
+  });
+});
