ebde1905941312b74dcdc66e90e0b3465c7e1b26
Max Lamb <maxlambcodes@gmail.com>
2020-06-15 16:32:54 -0700

    auction list tested and passing

--- __tests__/auction.test.js
+++ __tests__/auction.test.js
@@ -3,8 +3,9 @@ const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 
-// const Auction = require('../lib/models/Auction');
-// const Bid = require('../lib/models/Bid');
+const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
+const Bid = require('../lib/models/Bid');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -19,6 +20,33 @@ describe('auction routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let user;
+  let bid;
+  let auction;
+
+  beforeEach(async() => {
+    user = await User.create({
+      email: 'steve@castiel.com',
+      password: 'salmondean'
+    });
+    
+    auction = await Auction.create({
+      user: user.id,
+      title: 'Cheap Sneakers',
+      description: 'Crappy Shoes',
+      quantity: 2,
+      endDate: Date.now()
+    });
+    bid = await Bid.create({
+      user: user.id,
+      auction: auction.id,
+      price: 205,
+      quantity: 1,
+      accepted: true
+    });
+
+  });
+
   afterAll(async() => {
     await mongoose.connection.close();
     return mongod.stop();
@@ -26,9 +54,9 @@ describe('auction routes', () => {
 
   it('creates an auction', () => {
     return request(app)
-      .post('/api/v1/Auction')
+      .post('/api/v1/Auctions')
+      .auth('steve@castiel.com', 'salmondean')
       .send({
-        user: 'steve@castiel.com',
         title: 'Cheap Sneakers',
         description: 'Crappy Shoes',
         quantity: 2,
@@ -36,7 +64,9 @@ describe('auction routes', () => {
       })
       .then(res => {
         expect(res.body).toEqual({
-          user: 'steve@castiel.com',
+          _id: expect.anything(),
+          id: expect.anything(),
+          user: user.id,
           title: 'Cheap Sneakers',
           description: 'Crappy Shoes',
           quantity: 2,
@@ -44,5 +74,52 @@ describe('auction routes', () => {
           __v: 0
         });
       });
+  });
+
+  it('gets an auction by id', async() => {
+
+    const auction = await Auction.create({
+      user: user.id,
+      title: 'Cheap Sneakers',
+      description: 'Crappy Shoes',
+      quantity: 2,
+      endDate: Date.now()
+    });
+    return request(app)
+      .get(`/api/v1/Auctions/${auction._id}`)
+      .auth('steve@castiel.com', 'salmondean')
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.anything(),
+            email: 'steve@castiel.com'
+          },
+          _id: expect.anything(),
+          id: expect.anything(),
+          title: 'Cheap Sneakers',
+          description: 'Crappy Shoes',
+          quantity: 2,
+          endDate: expect.anything(),
+          __v: 0
+        });
+      }); 
+  });
+
+  it('gets a list of auctions', async() => {
+    return request(app)
+      .get('/api/v1/Auctions')
+      .auth('steve@castiel.com', 'salmondean')
+      .then(res => {
+        expect(res.body).toEqual([{
+          user: user.id,
+          _id: expect.anything(),
+          id: expect.anything(),
+          title: 'Cheap Sneakers',
+          description: 'Crappy Shoes',
+          quantity: 2,
+          endDate: expect.anything(),
+          __v: 0
+        }]);
+      }); 
   });
 });
